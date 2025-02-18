3b5935cae9f1af781d155463b6bd645635479a37
kenrique100 <ngwakenri2016@gmail.com>
2025-02-18 18:36:34 -0500

    added routes for swagger endpoints

--- src/main/java/com/akentech/microservices/routes/Routes.java
+++ src/main/java/com/akentech/microservices/routes/Routes.java
@@ -15,6 +15,16 @@ public class Routes {
                         .uri("http://localhost:8080"))
                 .build();
     }
+
+    @Bean
+    public RouteLocator productServiceSwaggerRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product-service-swagger", r -> r.path("/aggregate/product-service/v3/api-docs")
+                        .filters(f -> f.rewritePath("/aggregate/product-service/v3/api-docs", "/v3/api-docs"))
+                        .uri("http://localhost:8080"))
+                .build();
+    }
+
     @Bean
     public RouteLocator orderServiceRoute(RouteLocatorBuilder builder) {
         return builder.routes()
@@ -22,10 +32,29 @@ public class Routes {
                         .uri("http://localhost:8085"))
                 .build();
     }
+
+    @Bean
+    public RouteLocator orderServiceSwaggerRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("order-service-swagger", r -> r.path("/aggregate/order-service/v3/api-docs")
+                        .filters(f -> f.rewritePath("/aggregate/order-service/v3/api-docs", "/v3/api-docs"))
+                        .uri("http://localhost:8085"))
+                .build();
+    }
+
     @Bean
     public RouteLocator inventoryServiceRoute(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("inventory-service", r -> r.path("/api/inventory/**")
+                        .uri("http://localhost:8087"))
+                .build();
+    }
+
+    @Bean
+    public RouteLocator inventoryServiceSwaggerRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("inventory-service-swagger", r -> r.path("/aggregate/inventory-service/v3/api-docs")
+                        .filters(f -> f.rewritePath("/aggregate/inventory-service/v3/api-docs", "/v3/api-docs"))
                         .uri("http://localhost:8087"))
                 .build();
     }
