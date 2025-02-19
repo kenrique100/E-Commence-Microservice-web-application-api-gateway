e4cca76585da19fbb089ce57ffed73acb2a79067
kenrique100 <ngwakenri2016@gmail.com>
2025-02-19 03:53:08 -0500

    refactor Routes and removed multiple beans

--- src/main/java/com/akentech/microservices/routes/Routes.java
+++ src/main/java/com/akentech/microservices/routes/Routes.java
@@ -9,50 +9,20 @@ import org.springframework.context.annotation.Configuration;
 public class Routes {
 
     @Bean
-    public RouteLocator productServiceRoute(RouteLocatorBuilder builder) {
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("product-service", r -> r.path("/api/product/**")
                         .uri("http://localhost:8080"))
-                .build();
-    }
-
-    @Bean
-    public RouteLocator productServiceSwaggerRoute(RouteLocatorBuilder builder) {
-        return builder.routes()
                 .route("product-service-swagger", r -> r.path("/aggregate/product-service/v3/api-docs")
                         .filters(f -> f.rewritePath("/aggregate/product-service/v3/api-docs", "/v3/api-docs"))
                         .uri("http://localhost:8080"))
-                .build();
-    }
-
-    @Bean
-    public RouteLocator orderServiceRoute(RouteLocatorBuilder builder) {
-        return builder.routes()
                 .route("order-service", r -> r.path("/api/order/**")
                         .uri("http://localhost:8085"))
-                .build();
-    }
-
-    @Bean
-    public RouteLocator orderServiceSwaggerRoute(RouteLocatorBuilder builder) {
-        return builder.routes()
                 .route("order-service-swagger", r -> r.path("/aggregate/order-service/v3/api-docs")
                         .filters(f -> f.rewritePath("/aggregate/order-service/v3/api-docs", "/v3/api-docs"))
                         .uri("http://localhost:8085"))
-                .build();
-    }
-
-    @Bean
-    public RouteLocator inventoryServiceRoute(RouteLocatorBuilder builder) {
-        return builder.routes()
                 .route("inventory-service", r -> r.path("/api/inventory/**")
                         .uri("http://localhost:8087"))
-                .build();
-    }
-
-    @Bean
-    public RouteLocator inventoryServiceSwaggerRoute(RouteLocatorBuilder builder) {
-        return builder.routes()
                 .route("inventory-service-swagger", r -> r.path("/aggregate/inventory-service/v3/api-docs")
                         .filters(f -> f.rewritePath("/aggregate/inventory-service/v3/api-docs", "/v3/api-docs"))
                         .uri("http://localhost:8087"))
